b02af3af02531779d57799fc23c1f28c6eba12fc
Fernando Mendoza <55853779+whiteknight-dev@users.noreply.github.com>
2024-02-06 16:50:09 -0600

    Add functionality to the navbar menu

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+(function () {
+    let navbar = document.querySelector("#navbar");
+    let hamburguer = document.querySelector("#hamburguer");
+
+    hamburguer.addEventListener('click', () => {
+        navbar.classList.toggle("open");
+    });
+})();
